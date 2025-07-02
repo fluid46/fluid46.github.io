70ba1527131f9613dcad6515def0981eaf637455
Your Name <you@example.com>
2025-07-02 14:06:16 +0100

    Made a lot of changes, mainly getting some CSS and working on a favicon

---
+++ lazy-commit.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+cd /home/dan/fluid46.github.io
+# Prompt for commit message
+read -rp "Enter commit message: " COMMIT_MESSAGE
+
+# Run git commands
+echo "Committing with message: $COMMIT_MESSAGE"
+git add .
+git commit -m "$COMMIT_MESSAGE"
+git push
